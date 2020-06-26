fb6f8c9c3e0a4e474f0ffb460ca3073af9a2f8b4
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-06-26 15:20:18 -0500

    set new question function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,6 @@
 // PLAN
 
-// Function to start quiz
-// -When start button is clicked, start timer deduction and display new question
-
-// Function for new question
-// -Displays random question from array in question area, displays new choices on buttons. 
+//Function for checking answers
 // -When choice is correct, display 'RIGHT' and move to next question.
 // -When choice is wrong, display 'WRONG', deduct time from timer, and move on to next question.
 
@@ -19,14 +15,22 @@ var timeLeft = document.querySelector("#countDownClock");
 var questionSpan = document.querySelector("#question");
 var choice1 = document.querySelector("#choice1");
 var choice2 = document.querySelector("#choice2");
-var choice3 = document.querySelector("#choice3");
-var choice4 = document.querySelector("#choice4");
 
 
-
+var questions = [
+    {
+        question: 'What color is the sky?',
+        answers: {a: 'red', b: 'blue'},
+        correct: 'blue'
+    },
+    {
+        question: 'What color is grass?',
+        answers: {a: 'green', b: 'purple'},
+        correct: 'green'
+    }
+];
 
 var startQuiz = function() {
-    newQuestion();
     var time = 10;
 
     var timeInterval = setInterval(function() {
@@ -37,23 +41,40 @@ var startQuiz = function() {
             timeLeft.textContent = '';
             clearInterval(timeInterval);
             questionSpan.textContent = "Quiz Over!";
-            choice1.textContent = "Press";
-            choice2.textContent = "start to";
-            choice3.textContent = "try";
-            choice4.textContent = "again!";            
+            choice1.textContent = "Press start";
+            choice2.textContent = "to try again!";         
             window.alert("Time's up!");
         }
     }, 1000);
+
+    setNewQuestion();
 };
 
-var newQuestion = function() {
-    questionSpan.textContent = "What color is the sky?";
-    choice1.textContent = "green";
-    choice2.textContent = "red";
-    choice3.textContent = "blue";
-    choice4.textContent = "orange";
+var setNewQuestion = function() {
+    var i = 0; 
+    if (i < questions.length) {
+        var newQuestion = questions[i].question;
+        var answerA = questions[i].answers.a
+        var answerB = questions[i].answers.b
+        questionSpan.textContent = newQuestion;
+        choice1.textContent = answerA;
+        choice2.textContent = answerB;
+       i++; 
+    };     
 };
 
+var checkAnswer1 = function() {
+    
 
 
+    // If the button's text content = the question's correct answer then display correct and setNewQuestion()
+    
+
+    // Else display WRONg and set new question
+
+    // Then set a new question
+    // setNewQuestion();
+}
+
+choice1.addEventListener("click", checkAnswer1);
 startButton.addEventListener("click", startQuiz);
