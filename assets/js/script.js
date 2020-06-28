1cf2f7d36d0bd37e80730ce294de3f446af56fee
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-06-28 14:26:58 -0500

    added two more choices for each question

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,8 @@ var timeLeft = document.querySelector("#countDownClock");
 var questionSpan = document.querySelector("#question");
 var choice1 = document.querySelector("#choice1");
 var choice2 = document.querySelector("#choice2");
+var choice3 = document.querySelector("#choice3");
+var choice4 = document.querySelector("#choice4");
 var rightWrong = document.querySelector("#rightWrong");
 
 var showHighScoreInitials = document.querySelector("#highScoreInitials");
@@ -25,28 +27,29 @@ var time = "";
 var questions = [
     {
         question: 'What color is the sky?',
-        answers: {a: 'red', b: 'blue'},
+        answers: {a: 'red', b: 'blue', c: 'green', d: 'orange'},
         correct: 'blue'
     },
     {
         question: 'What color is grass?',
-        answers: {a: 'green', b: 'purple'},
+        answers: {a: 'green', b: 'purple', c: 'blue', d: 'orange'},
         correct: 'green'
     },
     {
         question: 'What is 4 + 4?',
-        answers: {a: '8', b: '7'},
+        answers: {a: '8', b: '7', c: '10', d: '44'},
         correct: '8'
     },
     {
         question: 'How many sideds are on a triangle?',
-        answers: {a: 'four', b: 'three'},
+        answers: {a: 'four', b: 'three', c: 'nine', d: 'one'},
         correct: 'three'
     }
 ];
 
 var startQuiz = function() {
-    time = 10;
+    currentQuestion = 0;
+    time = 20;
 
     var timeInterval = setInterval(function() {
         if (time > 0 && currentQuestion < questions.length) {
@@ -59,6 +62,8 @@ var startQuiz = function() {
     startButton.setAttribute("disabled", "disabled");
     choice1.removeAttribute("disabled");
     choice2.removeAttribute("disabled");
+    choice3.removeAttribute("disabled");
+    choice4.removeAttribute("disabled");
     setNewQuestion();
 };
 
@@ -66,12 +71,16 @@ var setNewQuestion = function() {
      
     if (currentQuestion < questions.length) {
         var newQuestion = questions[currentQuestion].question;
-        var answerA = questions[currentQuestion].answers.a
-        var answerB = questions[currentQuestion].answers.b
+        var answerA = questions[currentQuestion].answers.a;
+        var answerB = questions[currentQuestion].answers.b;
+        var answerC = questions[currentQuestion].answers.c;
+        var answerD = questions[currentQuestion].answers.d;
 
         questionSpan.textContent = newQuestion;
         choice1.textContent = answerA;
         choice2.textContent = answerB;
+        choice3.textContent = answerC;
+        choice4.textContent = answerD;
     } else {
         quizEnd();
     }     
@@ -90,7 +99,6 @@ var checkAnswer1 = function() {
     }
     // Then set a new question
     currentQuestion++;
-    console.log(currentQuestion);
     setNewQuestion();
 };
 
@@ -107,18 +115,54 @@ var checkAnswer2 = function() {
     }
     // Then set a new question
     currentQuestion++;
-    console.log(currentQuestion);
+    setNewQuestion();
+};
+
+var checkAnswer3 = function() {
+    var text = choice3.textContent
+    // If the button's text content = the question's correct answer then display correct and setNewQuestion()
+    if (questions[currentQuestion].correct === text) {
+        rightWrong.textContent = "RIGHT!"
+    }
+    // Else display WRONG and subtract 2 from time
+    else {
+        rightWrong.textContent = "WRONG!"
+        time = time - 2;
+    }
+    // Then set a new question
+    currentQuestion++;
+    setNewQuestion();
+};
+
+var checkAnswer4 = function() {
+    var text = choice4.textContent
+    // If the button's text content = the question's correct answer then display correct and setNewQuestion()
+    if (questions[currentQuestion].correct === text) {
+        rightWrong.textContent = "RIGHT!"
+    }
+    // Else display WRONG and subtract 2 from time
+    else {
+        rightWrong.textContent = "WRONG!"
+        time = time - 2;
+    }
+    // Then set a new question
+    currentQuestion++;
     setNewQuestion();
 };
 
 var quizEnd = function() {
     
     questionSpan.textContent = "Quiz Over!";
-    choice1.textContent = "Press start";
-    choice2.textContent = "to try again!";         
+    choice1.textContent = "Press";
+    choice2.textContent = "start";
+    choice3.textContent = "to try";
+    choice4.textContent = "again!";
+    rightWrong.textContent = "Thanks for playing!"           
     startButton.removeAttribute("disabled");
     choice1.setAttribute("disabled", "disabled");
     choice2.setAttribute("disabled", "disabled");
+    choice3.setAttribute("disabled", "disabled");
+    choice4.setAttribute("disabled", "disabled");
 
     window.alert("Quiz Over!");
     var score = time; 
@@ -137,4 +181,6 @@ var quizEnd = function() {
 
 choice1.addEventListener("click", checkAnswer1);
 choice2.addEventListener("click", checkAnswer2);
+choice3.addEventListener("click", checkAnswer3);
+choice4.addEventListener("click", checkAnswer4);
 startButton.addEventListener("click", startQuiz);
